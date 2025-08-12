c494d5d599bf58a3a30e1638b37f4ab64268daea
OscarCode442 <102172374+OscarCode442@users.noreply.github.com>
2025-08-12 16:05:47 +0100

    Create user

---
+++ users.py
@@ -0,0 +1,5 @@
+user_data = [
+    {"username": "alice", "password": 1234, "auth_level": 1},
+    {"username": "bob", "password": 5678, "auth_level": 2},
+    {"username": "carol", "password": 9012, "auth_level": 3}
+]
